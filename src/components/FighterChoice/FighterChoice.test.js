7dafa749ff62667f1fee37277f62996443449d54
Edward Runkel <eddierunkel@gmail.com>
2020-10-17 22:44:11 -0400

    Updated tests and snapshots

--- src/components/FighterChoice/FighterChoice.test.js
+++ src/components/FighterChoice/FighterChoice.test.js
@@ -14,22 +14,45 @@ describe('<FighterChoice>', () => {
     expect(container.html()).toMatchSnapshot();
   });
 
-  describe('when the fighter choice is clicked', () => {
-    it('should call onClick', () => {
-      const fighterChoice = container.find('.FighterChoice');
-      fighterChoice.simulate('click');
+  describe('the vertical fighter choice', () => {
+    describe('when the fighter choice is clicked', () => {
+      it('should call onClick', () => {
+        const fighterChoice = container.find('.FighterChoice.vertical');
+        fighterChoice.simulate('click');
 
-      expect(props.onClick.called).toBe(true);
+        expect(props.onClick.called).toBe(true);
+      });
+    });
+
+    describe('when leftFacing is true', () => {
+      const container = shallow(<FighterChoice leftFacing={true} {...props} />);
+
+      it('should add the class name appropriately', () => {
+        const fighterChoice = container.find('.FighterChoice.vertical');
+
+        expect(fighterChoice.hasClass('faceLeft')).toBe(true);
+      })
     });
   });
 
-  describe('when leftFacing is true', () => {
-    const container = shallow(<FighterChoice leftFacing={true} {...props} />);
+  describe('the horizontal fighter choice', () => {
+    describe('when the fighter choice is clicked', () => {
+      it('should call onClick', () => {
+        const fighterChoice = container.find('.FighterChoice.horizontal');
+        fighterChoice.simulate('click');
 
-    it('should add the class name appropriately', () => {
-      const fighterChoice = container.find('.FighterChoice');
+        expect(props.onClick.called).toBe(true);
+      });
+    });
 
-      expect(fighterChoice.hasClass('faceLeft')).toBe(true);
-    })
+    describe('when leftFacing is true', () => {
+      const container = shallow(<FighterChoice leftFacing={true} {...props} />);
+
+      it('should add the class name appropriately', () => {
+        const fighterChoice = container.find('.FighterChoice.horizontal');
+
+        expect(fighterChoice.hasClass('faceLeft')).toBe(true);
+      })
+    });
   });
 });
